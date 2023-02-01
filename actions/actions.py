6d912961d58e1854149c804d2e54adffb4f69dee
pizzita <sandro.palau@espam.edu.ec>
2023-02-01 00:15:05 -0500

    notice the domain actions error

--- actions/actions.py
+++ actions/actions.py
@@ -8,20 +8,25 @@
 # This is a simple example for a custom action which utters "Hello World!"
 
 from typing import Any, Text, Dict, List
+
 #
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+
 #
 #
 
+# i see domain is not working probably rasa_sdk modules were not imported
 class ActionSaveConversation(Action):
-
     def name(self) -> Text:
         return "action_save_conversation"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
 
         print(tracker.event)
 
